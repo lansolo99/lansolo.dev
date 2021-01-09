c9937a7a9e6df64581c0eb6cfdf00ababfe08c95
lansolo99 <lansolo99@hotmail.fr>
2021-01-09 15:35:19 +0100

    Add webfonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       inset: {
         '1/2': '50%',
       },
+      fontFamily: {
+        body: ['Karla', 'sans-serif'],
+        heading: ['Neue Machina', 'sans-serif'],
+      },
       spacing: {
         '1/2': '50%',
         '1/3': '33.333333%',
