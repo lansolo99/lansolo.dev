813f5d265e73c8bc7ad2bca59d25fc34a181e6de
lansolo99 <lansolo99@hotmail.fr>
2021-01-12 09:54:33 +0100

    Set working tags within post page

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <article class="container relative mx-auto text-white md:mt-8">
-      <!-- Image -->
+    <article class="container relative mx-auto text-white md:mt-8 md:px-8">
+      <!-- Cover Image(s) -->
       <div class="mx-auto" :class="{ 'max-w-680': post.type === 'article' }">
         <div class="relative w-full mx-auto overflow-hidden bg-red-700 pb-6/12">
           <nuxt-image
+            v-for="(image, i) in setCoverImagesSrc"
+            :key="i"
             class="absolute inset-0 object-cover"
-            :src="post.imgCover"
-            :alt="post.title"
+            :src="image.src"
+            :alt="image.title"
             :placeholder="true"
-            sizes="640,768:768"
+            :sizes="setCoverImageSizes"
           />
         </div>
       </div>
@@ -18,7 +20,7 @@
       <div class="p-4 mx-auto md:mt-5 max-w-680">
         <!-- Title -->
         <h1 class="text-2xl leading-snug md:text-3xl font-heading">
-          Nuxt + Firebase – part 0 – serie introduction
+          {{ post.title }}
         </h1>
 
         <!-- Tags -->
@@ -31,7 +33,12 @@
               :key="i"
               class="relative px-1 mt-2"
             >
-              <VFiltersButton :label="tag" :selected="false" context="post" />
+              <VFiltersButton
+                :label="tag"
+                :selected="false"
+                context="post"
+                @setTag="setTag"
+              />
             </div>
           </div>
         </div>
@@ -50,12 +57,49 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   name: 'Post',
   async asyncData({ $content, params }) {
     const post = await $content('posts', params.slug).fetch()
 
     return { post }
+  },
+
+  computed: {
+    setCoverImagesSrc() {
+      switch (this.post.type) {
+        case 'article':
+          return [
+            {
+              title: this.post.title,
+              src: this.post.imgCover,
+            },
+          ]
+        case 'design':
+          return this.post.imgList
+      }
+      return ''
+    },
+    setCoverImageSizes() {
+      switch (this.post.type) {
+        case 'article':
+          return '640,640:768'
+        case 'design':
+          return '640,640:768,768:1024,1024:1280,1280:1536'
+      }
+      return ''
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setSelectedTag: 'SET_SELECTED_TAGS',
+    }),
+    setTag(label) {
+      this.setSelectedTag([label])
+      this.$router.push('/')
+    },
   },
   head() {
     return {
